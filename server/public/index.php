b26eab468c977d996b6bacf311eab35a85f62279
Konstantin Aktuganov <k.aktuganov@vigbo.com>
2026-06-08 08:56:22 +0300

    feat: 301-редирект тестового стенда на основной сайт

--- server/public/index.php
+++ server/public/index.php
@@ -4,6 +4,13 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Перманентный редирект тестового стенда на основной сайт.
+// Срабатывает только по хосту тестового домена, поэтому на проде не зациклит.
+if (($_SERVER['HTTP_HOST'] ?? '') === 'museum.in-site.ru') {
+    header('Location: https://cadet-museum38.ru' . ($_SERVER['REQUEST_URI'] ?? '/'), true, 301);
+    exit;
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
